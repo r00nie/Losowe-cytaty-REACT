04c04d7456a3bae004ab6209dfaefd51abb14842
wackomagiciann <banasiakkacperr@gmail.com>
2022-07-08 17:17:19 +0200

    Changin structure and style of component

--- random-quote-app/src/App.js
+++ random-quote-app/src/App.js
@@ -1,12 +1,52 @@
 import React from "react";
+import axios from 'axios';
 
 import "./App.css";
 
 
 class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.fetchQuote = this.fetchQuote.bind(this);
+    }
+
+    state = {
+        text: "",
+        author: ""
+    };
+    
+    componentDidMount(){
+        this.fetchQuote();
+    }
+
+    fetchQuote(){
+        fetch("https://type.fit/api/quotes")
+            .then((res) => res.json())
+            .then(((json) => {
+                let randomNum = Math.floor(Math.random() * json.length);
+                let data = json[randomNum];
+                this.setState({
+                    text: data.text,
+                    author: data.author
+                })
+            }))
+    };
+
+    
+
     render(){
         return (
-            <h1>First Component</h1>
+            <div className="App">
+                <div className="quote-box">
+                    <p id="text-p">{this.state.text}</p>
+                    <p id="author-p">- {this.state.author}</p>
+                    <div className="btn-container">
+                        <button className="get-quote-btn" onClick={this.fetchQuote}>Get quote</button>
+                        <a href="" className="twitter-btn">Twitter</a>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
